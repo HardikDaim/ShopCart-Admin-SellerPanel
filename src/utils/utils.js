077b231339499227fdfcc503dc39fa50ffc85b4d
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-06-27 05:47:50 +0530

    Fix Socket export

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,4 +4,4 @@ const socketUrl = process.env.NODE_ENV === 'production'
   ? 'https://shop-cart-backend-green.vercel.app' 
   : 'http://localhost:4000';
 
-const socket = io(socketUrl);
+export const socket = io(socketUrl);
